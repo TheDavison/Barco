f26aea1835f27ea5f306282025d7ffa37e5e4acd
David <davidchengzijie@gmail.com>
2022-05-22 17:35:36 +0200

    intento de mostrar users siendo admins

--- assets/components/Users.jsx
+++ assets/components/Users.jsx
@@ -4,15 +4,54 @@ import React, { useEffect, useState } from 'react'
 const Users = () => {
     const [users, setUsers] = useState([])
 
-    useEffect( async () => {
-        await axios.get('user/list')
-            .then((response) => )
-      
-      
+    let getUsuarios = () => {
+        axios.get('user/list')
+            .then((response) => {                
+                // for(let usuario in response.data.data){
+                    // let { id, username, roles, donations } = response.data.data[usuario];
+                    // let nextUser = { id, username, roles, donations };
+                    // setUsers(prev => [...prev, nextUser] );
+                    let { data } = response.data;
+                    setUsers(data)
+                    console.log(data);
+                    console.log(users)
+                // }
+                console.log(users);
+                
+            })
+    // console.log(users);
+    }
+
+    useEffect(() => {
+        getUsuarios();
     }, [])
       
     return (
-        <div>Listado de donaciones</div>
+        <div>
+            <h2>Listado de usuarios</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Username</th>
+                        <th>Roles</th>
+                        <th>Donations</th>
+                    </tr>
+                </thead>
+
+                <tbody>   
+                    {/* {users.map((user, key) =>(
+                        <tr key={key}>
+                            <td>{user.id}</td>;
+                            <td>{user.username}</td>;
+                            <td>{user.roles}</td>;
+                            <td>{user.donations}</td>;
+                        </tr>
+                    ))} */}
+
+                </tbody>
+            </table>
+        </div>
+        
     )
 }
 
