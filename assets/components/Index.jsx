60edb7c69ed07f459f97107ef9fc3f8f17317087
ManuGL01 <manugonzalez400@gmail.com>
2022-05-28 21:05:33 +0200

    index changed

--- assets/components/Index.jsx
+++ assets/components/Index.jsx
@@ -4,7 +4,12 @@ import "../styles/Index.css";
 const Index = ({currentUser}) => {
   // console.log(currentUser);
   return (
-    <div>Hola, no se si esta funcionando {currentUser }</div>
+    <div className="index__container">
+      <div className="index__cam">Aquí irá la retransimisión de la cámara</div>
+      {currentUser ? <div className="index__donador">Donador 1</div> : ""}
+      {currentUser ? <div className="index__donador">Donador 2</div> : ""}
+      {currentUser ? <div className="index__donador">Donador 3</div> : ""}
+    </div>
   )
 }
 
