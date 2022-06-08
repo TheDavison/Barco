668ca6f0d2a58f6940ece876362b549f472c6082
David <davidchengzijie@gmail.com>
2022-06-09 00:24:44 +0200

    donaciones checkbox y er

--- assets/components/DonationForm.jsx
+++ assets/components/DonationForm.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import "../styles/DonationForm.css";
 import axios from "axios";
-import bricoADD from '../img/bricodepotADD.jpg';
+import bricoADD from "../img/bricodepotADD.jpg";
 import { useNavigate } from "react-router-dom";
 
 const DonationForm = () => {
   const [type, setType] = useState("MasterCard");
-  const [quantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(0);
   const [cardNumber, setCardNumber] = useState("");
   const [cardHolder, setCardHolder] = useState("");
   const [cardDate, setCardDate] = useState("");
   const [cardCVV, setCardCVV] = useState("");
   const [discordUsername, setDiscordUsername] = useState("");
+  const [terms, setTerms] = useState(false);
+  const [error, setError] = useState("");
 
   let navigate = useNavigate();
 
@@ -31,51 +33,91 @@ const DonationForm = () => {
     } else if (e.target.name === "cantidad") {
       setQuantity(e.target.value);
     }
+
+    setError("");
   };
 
   const handleTypeChange = (type) => {
     setType(type);
+    setError("");
   };
 
-  // const checkInputs = () => {
-  //   let correctos = true;
-  //   if(type != "Visa" || type != "MasterCard"){
-  //     correctos = false;
-  //   }
-
-  //   if(quantity <= 0){
-  //     correctos = false;
-  //   }
-
-  //   expresion = /((\d{4} ){3}\d{4}|^\d{16}$)/gm;
-  //   if(expresion.text(cardNumber)){
-  //     correctos = false;
-  //   }
-
-  //   expresion = //;
-  //   expresion = //;
-  //   expresion = //;
-  //   expresion = //;
-  //   expresion = //;
-  //   expresion = //;
-  //   expresion = //;
-  // }
+  const handleAcceptTerms = () => {
+    setTerms(!terms);
+    setError("");
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // checkInputs();
 
-    let donatedSuccesfully = await addDonation();
+    if (checkInputs()) {
+      let donatedSuccesfully = await addDonation();
 
-    if(donatedSuccesfully){
-      setQuantity(1);
-      setCardNumber("");
-      setCardHolder("");
-      setCardDate("");
-      setCardCVV("");
-      setDiscordUsername("");
-      navigate("/", { replace: true });
+      if (donatedSuccesfully) {
+        setQuantity(1);
+        setCardNumber("");
+        setCardHolder("");
+        setCardDate("");
+        setCardCVV("");
+        setDiscordUsername("");
+        navigate("/", { replace: true });
+      }
     }
+  };
+
+  const checkInputs = () => {
+    let mayContinue = true;
+
+    if (!type === "MasterCard" || !type === "Visa") {
+      mayContinue = false;
+      setError("Solo puede pagar via MasterCard o Visa.");
+    }
+
+    if (quantity <= 0) {
+      mayContinue = false;
+      setError("No puede donar una cantidad menor o igual a 0€.");
+    }
+
+    expresion = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/gm;
+    if (!expresion.test(cardNumber)) {
+      mayContinue = false;
+      setError("Número de tarjeta errónea.");
+    }
+
+    expresion = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/;
+    if (!expresion.test(cardHolder)) {
+      mayContinue = false;
+      setError("Nombre erróneo.");
+    }
+
+    expresion = /^(\d{2}\/\d{4})$/gm;
+    if (!expresion.test(cardDate)) {
+      mayContinue = false;
+      setError("Fecha formato MM/YYYY.");
+    }
+
+    expresion = /^(\d{3})$/gm;
+    if (!expresion.test(cardCVV)) {
+      mayContinue = false;
+      setError("Tres numeros de detrás de la tarjeta.");
+    }
+
+    expresion = /^([a-zA-Z0-9]{2,32})#(\d{4})$/gm;
+    if(discordUsername.trim().length > 0){
+      if (!expresion.test(discordUsername)) {
+        mayContinue = false;
+        setError("No ha escrito bien su id de discord.");
+      }
+    }else{
+      setDiscordUsername("NoDiscord")
+    }
+
+    if (!terms) {
+      mayContinue = false;
+      setError("Acepte los términos antes de continuar por favor.");
+    }
+
+    return mayContinue;
   };
 
   const addDonation = async () => {
@@ -90,7 +132,7 @@ const DonationForm = () => {
         cardCVV: cardCVV,
         discordUsername: discordUsername,
       })
-      .then(haveDonated = true)
+      .then((haveDonated = true))
       .catch((error) => {
         console.log(error);
       });
@@ -100,7 +142,9 @@ const DonationForm = () => {
   return (
     <div className="donation-form__container">
       <aside className="donation-form__add">
-        <a href="https://www.bricodepot.es/"><img src={bricoADD} alt="publicidad de bricodepot"/></a>
+        <a href="https://www.bricodepot.es/">
+          <img src={bricoADD} alt="publicidad de bricodepot" />
+        </a>
       </aside>
 
       <div className="donation-form__main">
@@ -109,14 +153,22 @@ const DonationForm = () => {
             <p>Métodos de pago</p>
             <div className="donation-form__payment-methods">
               <p
-                className="donation-form__payment-method"
+                className={
+                  type == "MasterCard"
+                    ? "donation-form__payment-method active-type"
+                    : "donation-form__payment-method"
+                }
                 name="donation-type"
                 onClick={() => handleTypeChange("MasterCard")}
               >
                 MasterCard
               </p>
               <p
-                className="donation-form__payment-method"
+                className={
+                  type == "Visa"
+                    ? "donation-form__payment-method active-type"
+                    : "donation-form__payment-method"
+                }
                 name="donation-type"
                 onClick={() => handleTypeChange("Visa")}
               >
@@ -124,7 +176,9 @@ const DonationForm = () => {
               </p>
             </div>
             <div className="donation-form__quantity">
-              <label htmlFor="cantidad">Cantidad que va a donar:</label>
+              <label htmlFor="cantidad" className="form-label">
+                Cantidad que va a donar:
+              </label>
               <input
                 type="number"
                 min="1"
@@ -144,7 +198,7 @@ const DonationForm = () => {
               className="donation-form"
             >
               <div className="form__number form-field">
-                <label htmlFor="card-number form-label">
+                <label htmlFor="card-number" className="form-label">
                   Número de la tarjeta:
                 </label>
                 <input
@@ -158,7 +212,7 @@ const DonationForm = () => {
               </div>
 
               <div className="form__holder form-field">
-                <label htmlFor="card-holder form-label">
+                <label htmlFor="card-holder" className="form-label">
                   Nombre del propietario de la tarjeta:
                 </label>
                 <input
@@ -172,7 +226,7 @@ const DonationForm = () => {
               </div>
 
               <div className="form__date form-field">
-                <label htmlFor="card-date form-label">
+                <label htmlFor="card-date" className="form-label">
                   Fecha de caducidad de la tarjeta:
                 </label>
                 <input
@@ -186,7 +240,7 @@ const DonationForm = () => {
               </div>
 
               <div className="form__cvv form-field">
-                <label htmlFor="card-cvv form-label">
+                <label htmlFor="card-cvv" className="form-label">
                   Numero de seguridad de la tarjeta:
                 </label>
                 <input
@@ -200,7 +254,7 @@ const DonationForm = () => {
               </div>
 
               <div className="form__discord-username form-field">
-                <label htmlFor="discord-username form-label">
+                <label htmlFor="discord-username" className="form-label">
                   Usurio de discord (Opcional):
                 </label>
                 <input
@@ -213,6 +267,19 @@ const DonationForm = () => {
                 />
               </div>
 
+              <div className="form__checkbox form-field">
+                <label htmlFor="accept-terms" className="form-label">
+                  Acceto los términos:{" "}
+                </label>
+                <input
+                  type="checkbox"
+                  id="accept-terms"
+                  onClick={handleAcceptTerms}
+                />
+              </div>
+
+              {error ? <p className="error_message">{error}</p> : ""}
+
               <div className="form-button">
                 <p onClick={handleSubmit}>Donar</p>
               </div>
@@ -222,7 +289,9 @@ const DonationForm = () => {
       </div>
 
       <aside className="donation-form__add">
-        <a href="https://www.bricodepot.es/"><img src={bricoADD} alt="publicidad de bricodepot"/></a>
+        <a href="https://www.bricodepot.es/">
+          <img src={bricoADD} alt="publicidad de bricodepot" />
+        </a>
       </aside>
     </div>
   );
